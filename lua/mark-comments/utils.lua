3bf6ba40199695a117efd342cdab6ce18a1380e7
Szymon Wieczorek <shateq21@gmail.com>
2026-02-07 13:41:11 +0100

    api: Provide way to disable/enable plugin fix: inconsistent parsing across different filetypes Purify utils

--- lua/mark-comments/utils.lua
+++ lua/mark-comments/utils.lua
@@ -1,21 +1,29 @@
 local M = {}
 
---- Return if buftype is empty which means normal buffer
+---Return buffer's filetype
 ---@param bufnr number
----@return boolean
-function M.is_empty_buftype(bufnr)
-  local bt = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
-
-  return bt == ""
+---@return filetype
+function M.get_ft(bufnr)
+  -- local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+  local ft = vim.bo[bufnr].filetype
+  return ft
 end
 
+---Return buffer's type
+---@param bufnr number
+---@return string
+function M.get_bt(bufnr)
+  -- local bt = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
+  local bt = vim.bo[bufnr].buftype
+  return bt
+end
+
+--- Returns true if treesitter captures comment on passed position
 ---@param bufnr number
 ---@param row number
 ---@param col number
 ---@return boolean
 function M.is_comment(bufnr, row, col)
-  -- TODO should we add fallback for no tresitter highlighter?
-  -- if vim.treesitter isn't available its probably nvim 0.5
   if vim.treesitter.highlighter.active[bufnr] then
     -- row: 0 indexed col: 0 indexed
     local capts = vim.treesitter.get_captures_at_pos(bufnr, row, col)
@@ -28,9 +36,12 @@ function M.is_comment(bufnr, row, col)
   end
 
   return false
+  -- TODO should we add fallback for no tresitter highlighter?
+  -- if vim.treesitter isn't available its probably nvim 0.5
 end
 
--- TODO: not only oil and telescope are affected
+--- Returns true if buffer isn't a 'working' buffer,
+--- that is a normal buffer for editing not provided by a plugin
 ---@param bufnr number
 ---@return boolean
 function M.is_special_buffer(bufnr)
@@ -45,9 +56,11 @@ function M.is_special_buffer(bufnr)
   local o = filetype == "oil"
 
   return p or r or o
+  -- TODO: not only oil and telescope are affected
   -- vim.api.nvim_buf_get_name(bufnr):match('^oil://') ~= nil
 end
 
+--- Checks if treesitter recognizes buffer bufnr language
 ---@param bufnr number
 ---@return boolean
 local function is_treesitter_lang(bufnr)
