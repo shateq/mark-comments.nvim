6e4089c5e0af7f5e9c3e73ad63d1d777ef7acf32
Szymon Wieczorek <shateq21@gmail.com>
2026-02-07 15:07:38 +0100

    implement basic picker for Every mark, we need picker for local only

--- lua/telescope/_extensions/mark-comments.lua
+++ lua/telescope/_extensions/mark-comments.lua
@@ -1,36 +1,67 @@
+-- M:a
 -- Telescope extension allowing to browse through created marks, aka chapters of the file
--- goto on onter
--- luafile %
 local has_telescope, telescope = pcall(require, "telescope")
 
 if not has_telescope then
-  error("Mark comments integration requires nvim-telescope/telescope.nvim")
+  error("This mark-comments.nvim integration requires nvim-telescope/telescope.nvim")
 end
 
 local Pickers = require "telescope.pickers"
-local make_entry = require "telescope.make_entry"
-local finders = require "telescope.finders"
+local Finders = require "telescope.finders"
 local Conf = require "telescope.config".values
 
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
--- local marks
+-- local marks (M:b)
 local marked = function(opts)
   opts = opts or {}
+  local bufnr = vim.api.nvim_get_current_buf()
+  local marks = vim.fn.getmarklist(bufnr)
+
+  if #marks < 1 then
+    vim.notify("No marks to search through", 3)
+    return
+  end
+
+  local results = {}
+  for _, m in ipairs(marks) do
+    table.insert(results, { m.mark:sub(2), m.pos })
+  end
+
+
   Pickers.new(opts, {
-    prompt_title = "Markers",
-    finder = finders.new_table {
-      results = { "a", "b", "c" }
+    prompt_title = "Mark prompt",
+    results_title = "Markers",
+    finder = Finders.new_table {
+      results = results,
+      entry_maker = function(entry)
+        return {
+          value = entry,
+          display = entry[1],
+          ordinal = entry[1],
+        }
+      end
     },
     sorter = Conf.generic_sorter(opts),
+    -- previewer = Conf.grep_previewer(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+
+        vim.cmd("'" .. selection.value[1])
+        -- Could also use vim.api.nvim_win_set_cursor()
+      end)
+      return true
+    end,
   }):find()
 end
 
+-- m:c
 marked(
   require("telescope.themes").get_dropdown {}
 )
 
 -- require('telescope').load_extension('fzy_native')
-
--- return telescope.register_extension({ exports = { ["mark-comments"] = marks, todo = marks } })
+return telescope.register_extension({ exports = { ["mark-comments"] = marked, marked = marked } })
