bfdf7903dfc39ed682e62aa9160769f627c4f36b
Simon <69470758+shateq@users.noreply.github.com>
2026-02-08 22:47:04 +0100

    Telescope.nvim integration (#1)
    * include boilerplate
    
    * api: Provide way to disable/enable plugin
    fix: inconsistent parsing across different filetypes
    Purify utils
    
    * implement basic picker for Every mark, we need picker for local only
    
    * add previewer & filter local marks
    
    * don't open picker without marks
    
    * fixed autocmd not running, experimented with lazy loading
    
    * improve test sources
    
    * save generated marks to a table
    
    it helps distinguish user set marks from plugin set marks
    
    * provide way to reset current buffer marks
    
    guttermarks integration, for debugging also
    
    * fix: marks not created on entering buffer from oil
    
    * feat: upstream show only generated marks
    
    * api: find unassigned mark
    
    * properly implement caching

---
+++ lua/telescope/_extensions/mark-comments.lua
@@ -0,0 +1,79 @@
+-- M:a
+-- Telescope extension allowing to browse through created marks, aka chapters of the file
+local has_telescope, telescope = pcall(require, "telescope")
+
+if not has_telescope then
+  error("This mark-comments.nvim integration requires nvim-telescope/telescope.nvim")
+end
+
+local Pickers = require "telescope.pickers"
+local Finders = require "telescope.finders"
+local Conf = require "telescope.config".values
+
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+-- local marks (M:b)
+local marked = function(opts)
+  opts = opts or {}
+  local bufnr = vim.api.nvim_get_current_buf()
+  local marks = vim.fn.getmarklist(bufnr)
+
+  local gen_marks = require("mark-comments").get_marks(bufnr)
+
+  local results = {}
+  for _, m in ipairs(marks) do
+    -- only letter mark
+    if m.mark:match("%a") then
+      local mname = m.mark:sub(2)
+
+      -- TODO configure with opts
+      -- only generated marks
+      if vim.tbl_contains(gen_marks, mname) then
+        table.insert(results, { mname, m.pos })
+      end
+    end
+  end
+
+  if #results < 1 then
+    vim.notify("No marks to search through", 3)
+    return
+  end
+
+  local fname = vim.api.nvim_buf_get_name(bufnr)
+  Pickers.new(opts, {
+    preview_title = "Current buffer",
+    prompt_title = "Mark prompt",
+    results_title = "Markers",
+
+    finder = Finders.new_table {
+      results = results,
+      entry_maker = function(entry)
+        return {
+          value = entry,
+          display = entry[1],
+          ordinal = entry[1],
+          filename = fname,
+          lnum = entry[2][2]
+        }
+      end
+    },
+
+    previewer = Conf.grep_previewer(opts),
+    sorter = Conf.generic_sorter(opts),
+
+    attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        -- Could also use vim.api.nvim_win_set_cursor()
+        vim.cmd("'" .. selection.value[1])
+      end)
+      return true
+    end,
+  }):find()
+end
+-- marked(require("telescope.themes").get_dropdown {})
+
+-- require('telescope').load_extension('mark-comments')
+return telescope.register_extension({ exports = { ["mark-comments"] = marked } })
